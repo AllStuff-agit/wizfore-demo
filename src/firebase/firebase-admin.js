285a8bf79536b98e387d73c24afb61af4ba50f90
AllStuff-agit <ulimyu428@gmail.com>
2025-04-27 18:38:28 +0900

    7

--- src/firebase/firebase-admin.js
+++ src/firebase/firebase-admin.js
@@ -1,21 +1,29 @@
 import * as admin from 'firebase-admin';
 
-const firebaseAdminConfig = {
-  privateKey: (process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY as string).replace(/\\n/g, '\n'),
-  clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-};
+const {
+  FIREBASE_PROJECT_ID,
+  FIREBASE_CLIENT_EMAIL,
+  FIREBASE_PRIVATE_KEY,
+  FIREBASE_DATABASE_URL
+} = process.env;
+
+// 필수 값이 없으면 에러를 던져 빠르게 인지하도록
+if (!FIREBASE_PROJECT_ID ||
+    !FIREBASE_CLIENT_EMAIL ||
+    !FIREBASE_PRIVATE_KEY ||
+    !FIREBASE_DATABASE_URL) {
+  throw new Error('Missing one of Firebase admin environment variables');
+}
 
 if (!admin.apps.length) {
-  try {
-    admin.initializeApp({
-      credential: admin.credential.cert(firebaseAdminConfig),
-      databaseURL: 'https://test-e2e5c-default-rtdb.asia-southeast1.firebasedatabase.app',
-
-    });
-  } catch (error) {
-    console.log('Firebase admin initialization error', error.stack);
-  }
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId:    FIREBASE_PROJECT_ID,
+      clientEmail:  FIREBASE_CLIENT_EMAIL,
+      privateKey:   FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    }),
+    databaseURL: FIREBASE_DATABASE_URL,
+  });
 }
 
 export default admin;
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,21 +2,24 @@ import { initializeApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDmuAmLG4o-gH6X3jFI1o5FG6HGiqpDX6M",
-  authDomain: "wizfore-demo.firebaseapp.com",
-  projectId: "wizfore-demo",
-  storageBucket: "wizfore-demo.firebasestorage.app",
-  messagingSenderId: "12717939021",
-  appId: "1:12717939021:web:261ce55355dd0cfdddff04",
-  databaseURL: "https://wizfore-demo-default-rtdb.asia-southeast1.firebasedatabase.app/"
+const {
+  NEXT_PUBLIC_FIREBASE_API_KEY,
+  NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  NEXT_PUBLIC_FIREBASE_APP_ID
+} = process.env;
 
-  // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+const firebaseConfig = {
+  
+  apiKey:            NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain:        NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId:         FIREBASE_PROJECT_ID,
+  storageBucket:     NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             NEXT_PUBLIC_FIREBASE_APP_ID
+
 };
 
 // Initialize Firebase
