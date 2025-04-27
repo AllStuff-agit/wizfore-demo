6f550381e07e64d836676af2f9c83280f7bbbdd2
AllStuff-agit <ulimyu428@gmail.com>
2025-04-27 19:28:34 +0900

    13

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,12 +13,12 @@ const {
 
 const firebaseConfig = {
   
-  apiKey:            NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain:        NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId:         NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket:     NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId:             NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyDmuAmLG4o-gH6X3jFI1o5FG6HGiqpDX6M",
+  authDomain: "wizfore-demo.firebaseapp.com",
+  projectId: "wizfore-demo",
+  storageBucket: "wizfore-demo.firebasestorage.app",
+  messagingSenderId: "12717939021",
+  appId: "1:12717939021:web:261ce55355dd0cfdddff04"
 
 };
 
