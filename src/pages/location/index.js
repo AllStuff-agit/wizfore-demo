781c16176916cc13db59a9b75e612a2831da56f6
AllStuff-agit <ulimyu428@gmail.com>
2025-04-28 16:47:25 +0900

    오시는 길 - 지도 임베디드

--- src/pages/location/index.js
+++ src/pages/location/index.js
@@ -79,7 +79,15 @@ export default function Location() {
             
             <div className={styles.mapWrapper}>
               <div className={styles.map}>
-                <img src="/images/map.jpg" alt="위즈포레 지도" />
+                <iframe
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3261.1234567890123!2d128.9917!3d35.1796!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzXCsDEwJzQ2LjYiTiAxMjDCsDU5JzM0LjEiRQ!5e0!3m2!1sko!2skr!4v1234567890!5m2!1sko!2skr"
+                  width="100%"
+                  height="100%"
+                  style={{ border: 0 }}
+                  allowFullScreen=""
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
               </div>
             </div>
           </div>
