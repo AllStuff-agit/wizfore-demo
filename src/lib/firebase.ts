e71a41a8616965ff510bd8a3a08e2006428e74c5
AllStuff-agit <ulimyu428@gmail.com>
2025-06-27 19:26:50 +0900

    주석 제거

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -17,17 +17,6 @@ export const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
-// const firebaseConfig = {
-  
-//   apiKey: "AIzaSyDmuAmLG4o-gH6X3jFI1o5FG6HGiqpDX6M",
-//   authDomain: "wizfore-demo.firebaseapp.com",
-//   projectId: "wizfore-demo",
-//   storageBucket: "wizfore-demo.firebasestorage.app",
-//   messagingSenderId: "12717939021",
-//   appId: "1:12717939021:web:261ce55355dd0cfdddff04"
-
-// };
-
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
